bf8691a3e98d66aa25245888a1d8730d59fe8e57
Tudor Hirtopanu <Tudorhi2003@gmail.com>
2023-01-08 14:02:17 +0000

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Tudor Hirtopanu on 08/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
